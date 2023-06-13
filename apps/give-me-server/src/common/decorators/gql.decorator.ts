f99ebef8e0c43d2729212dff4512b2fda0bc5196
Nosenko Ilya <pitbullstar96@gmail.com>
2023-06-13 23:17:59 +0300

    декоратор запроса для graphql

--- apps/give-me-server/src/common/decorators/gql.decorator.ts
+++ apps/give-me-server/src/common/decorators/gql.decorator.ts
@@ -1,9 +1,13 @@
 import { createParamDecorator } from '@nestjs/common';
-import { Response } from 'express';
+import {Request, Response} from 'express';
 import { User } from '@prisma/client';
 
 export const ResGql = createParamDecorator(
-  (data, [root, args, ctx, info]): Response => ctx.res,
+    (data, [root, args, ctx, info]): Response => ctx.res,
+);
+
+export const ReqGql = createParamDecorator(
+    (data, [root, args, ctx, info]): Request => ctx.req,
 );
 
 export const GqlUser = createParamDecorator(
